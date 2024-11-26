6004562e741faffa7bac0bf4bdc6f957f1d9a02f
José de los santos <delossantosramosjose@gmail.com>
2024-11-26 19:01:36 +0100

    fonts added to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,41 @@
 /** @type {import('tailwindcss').Config} */
 
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        celestina: ['Herr Von Muellerhoff'],
+        roboto: ['Roboto'],
+      },
+    },
   },
 
-  plugins: [],
+  plugins: [
+
+    plugin(function ({ addBase }) {
+      addBase({
+        '@font-face': [
+
+          {
+            fontFamily: 'Herr Von Muellerhoff',
+            fontWeight: '400',
+            src: 'url(../public/assets/fonts/HerrVonMuellerhoff/HerrVonMuellerhoff-Regular.ttf) format("truetype")',
+          },
+
+          {
+            fontFamily: 'Roboto',
+            fontWeight: '400',
+            src: 'url(../public/assets/fonts/Roboto/Roboto-Regular.ttf) format("truetype")',
+          }
+
+        ]
+      })
+    })
+
+  ],
 }
